d30fd8f3988c9bd08708967bfaaf0a96ef32a16d
Lucca Yuki <kelcoolg@gmail.com>
2026-05-18 08:38:03 -0300

    Projeto Finalizado

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-import { prisma } from  '../libs/prisma'
-import { createUser, createUsers, getAllUsers } from '../services/user'
-import { get } from 'http';
+import { createUser, createUsers, getAllUsers, getUserByEmail } from '../services/user'
 
 export const mainRouter = Router();
 
@@ -49,5 +47,14 @@ mainRouter.get('/users', async (req, res) => {
         res.json({ users })
     } else {
         res.status(500).json({ error: 'Error fetching users' })
+    }
+})
+
+mainRouter.get('/user', async (req, res) => {
+    const user = await getUserByEmail('charlie.brown@example.com')
+    if (user) {
+        res.json( { user })
+    } else {
+        res.status(404).json({ error: 'User not found' })
     }
 })
